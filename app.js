28bf40b009e58ea26e9cef27f276e10e1eab5830
moiz <amkhatri89@gmail.com>
2023-11-17 17:21:49 +0500

    objects formation for cars

---
+++ app.js
@@ -0,0 +1,256 @@
+var cars = {
+    honda: {
+        model: {
+            civic: {
+                make: "Honda",
+                model: "Civic",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+
+            },
+            crv: {
+                make: "Honda",
+                model: "CR-V",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            hrv: {
+                make: "Honda",
+                model: "HR-V",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            accord: {
+                make: "Honda",
+                model: "Accord",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            city: {
+                make: "Honda",
+                model: "City",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+
+        }
+
+    },
+    nissan: {
+        model: {
+            sentra: {
+                make: "Nissan",
+                model: "Sentra",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+
+            },
+            altima: {
+                make: "Nissan",
+                model: "Altima",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            juke: {
+                make: "Nissan",
+                model: "Juke",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            gtr: {
+                make: "Nissan",
+                model: "GT_R",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            micra: {
+                make: "Nissan",
+                model: "Micra",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            dayz: {
+                make: "Nissan",
+                model: "Dayz",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+
+        }
+    },
+    toyota: {
+        model: {
+            corolla: {
+                make: "Toyota",
+                model: "Corolla",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+
+            },
+            camry: {
+                make: "Toyota",
+                model: "Camry",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            prius: {
+                make: "Toyota",
+                model: "Prius",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            yaris: {
+                make: "Toyota",
+                model: "Yaris",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            fortuner: {
+                make: "Toyota",
+                model: "Fortuner",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            crown: {
+                make: "Toyota",
+                model: "Crown",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+        }
+    },
+    suzuki: {
+        model: {
+            swift: {
+                make: "Suzuki",
+                model: "Swift",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+
+            },
+            alto: {
+                make: "Suzuki",
+                model: "Alto",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            jimmy: {
+                make: "Suzuki",
+                model: "Jimmy",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            cultus: {
+                make: "Suzuki",
+                model: "Cultus",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            ciaz: {
+                make: "Suzuki",
+                model: "Ciaz",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+        }
+    },
+    kia: {
+        model: {
+            sorento: {
+                make: "KIA",
+                model: "Sorento",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            sportage: {
+                make: "KIA",
+                model: "Sportage",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            picanto: {
+                make: "KIA",
+                model: "Picanto",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+        },
+        
+    },
+    daihatsu: {
+        model: {
+            mira: {
+                make: "Daihatsu",
+                model: "Mira",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            hijet: {
+                make: "Daihatsu",
+                model: "Hijet",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            move: {
+                make: "Daihatsu",
+                model: "Move",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+        }
+    },
+    audi: {
+        model: {
+            a3: {
+                make: "Audi",
+                model: "A3",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            a4: {
+                make: "Audi",
+                model: "A4",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            },
+            a5: {
+                make: "Audi",
+                model: "A5",
+                year: 2023,
+                color: ["White", "Black", "Red", "Navy Blue", "Steel Grey"],
+                transmission: ["Automatic", "Manual"]
+            }
+        }
+    }
+}
+
